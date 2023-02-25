4a7a21959e988b9be71590c8b9c5f979cb805389
Eshban Suleman <eshban9492@gmail.com>
2023-02-25 15:52:30 +0500

    Updated project setup script

--- setup_project.sh
+++ setup_project.sh
@@ -2,6 +2,6 @@
 
 git clone https://github.com/jmnwong/NSL-KDD-Dataset.git
 
-wget -O training_attack_types.txt http://kdd.ics.uci.edu/databases/kddcup99/training_attack_types
+wget -O NSL-KDD-Dataset/training_attack_types.txt http://kdd.ics.uci.edu/databases/kddcup99/training_attack_types
 
 pip install -r requirements.txt
